ce811d14865a1162a2909ab2bdcb82c8e6119a79
Brittany <brithamilton1@gmail.com>
2017-07-18 20:38:50 -0400

    final

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//how to submit the form into an object
+var seatsReserved={};
+
+
 $(function(){
   $('td').click(function(){
     $(this).toggleClass('blue');
@@ -13,22 +17,24 @@ $('form').submit(function(event){
   var mobile  = $(this).find('#mobile').val();
   var reservedSeats = '';
   var $tableData = $('td');
-  for (var i = 0; i < $tableData.length; i++) {
-    if($($tableData[i]).hasClass('blue')){
-      $($tableData[i]).removeClass('blue');
-      $($tableData[i]).addClass('reserved');
-      $($tableData[i]).html('X');
-      reservedSeats += ' ' + $($tableData[i]).attr('id') + ',';
-    }
-  }
-  // Array.from($('td')).forEach(function(seat) {
-  //   if($(seat).hasClass('blue')){
-  //     $(seat).removeClass('blue');
-  //     $(seat).addClass('reserved');
-  //     $(seat).html('X');
+  // for (var i = 0; i < $tableData.length; i++) {
+  //   if($($tableData[i]).hasClass('blue')){
+  //     $($tableData[i]).removeClass('blue');
+  //     $($tableData[i]).addClass('reserved');
+  //     $($tableData[i]).html('X'); //taking the text out of the box, will show X
+  //     reservedSeats += ' ' + $($tableData[i]).attr('id') + ',';
   //   }
-  // });
+  // }
+  Array.from($('td')).forEach(function(seat) {
+    if($(seat).hasClass('blue')){
+      $(seat).removeClass('blue');
+      $(seat).addClass('reserved');
+      $(seat).html('X');
+    }
+  });
+
   alert(name + ' your reserved seats' + reservedSeats);
+  // $(this).trigger("reset");  --to reset the form
 });
 
 
