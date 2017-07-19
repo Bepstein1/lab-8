bfceda7622157e59cb82dbacc8e842a2804a21bc
Brittany <brithamilton1@gmail.com>
2017-07-18 20:02:35 -0400

    final

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 $(function(){
-$('td').click(function(){
-  $(this).toggleClass('blue');
-});
-  $('#submitbutton').on('click', function(){
-    delay(1500).remove();
-    //  alert('Your seat is reserved');
+  $('td').click(function(){
+    $(this).toggleClass('blue');
   });
+
+$('form').submit(function(event){
+	// prevent default stop the form from
+	// refreshing the page
+	event.preventDefault();
+	// #name is the id of an input field
+	// val is how you get the value
+	var name  = $(this).find('#name').val();
+  var mobile  = $(this).find('#mobile').val();
+  var reservedSeats = '';
+  var $tableData = $('td');
+  for (var i = 0; i < $tableData.length; i++) {
+    if($($tableData[i]).hasClass('blue')){
+      $($tableData[i]).removeClass('blue');
+      $($tableData[i]).addClass('reserved');
+      $($tableData[i]).html('X');
+      reservedSeats += ' ' + $($tableData[i]).attr('id') + ',';
+    }
+  }
+  // Array.from($('td')).forEach(function(seat) {
+  //   if($(seat).hasClass('blue')){
+  //     $(seat).removeClass('blue');
+  //     $(seat).addClass('reserved');
+  //     $(seat).html('X');
+  //   }
+  // });
+  alert(name + ' your reserved seats' + reservedSeats);
+});
+
+
 });
